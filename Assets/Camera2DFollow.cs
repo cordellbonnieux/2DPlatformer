eba7e6b092382d72173d513c19cfff73fbf5d859
Cordell Bonnieux <cordellbonnieux@gmail.com>
2021-02-15 13:25:49 -0800

    Added a respawn when the player dies.

--- Assets/Camera2DFollow.cs
+++ Assets/Camera2DFollow.cs
@@ -17,6 +17,8 @@ namespace UnityStandardAssets._2D
         private Vector3 m_CurrentVelocity;
         private Vector3 m_LookAheadPos;
 
+        float nextTimeToSearch = 0;
+
         // Use this for initialization
         private void Start()
         {
@@ -29,8 +31,10 @@ namespace UnityStandardAssets._2D
         // Update is called once per frame
         private void Update()
         {
-            if (target == null)
+            if (target == null){
+                FindPlayer ();
                 return;
+            }
                 
             // only update lookahead pos if accelerating or changed direction
             float xMoveDelta = (target.position - m_LastTargetPosition).x;
@@ -54,6 +58,14 @@ namespace UnityStandardAssets._2D
             transform.position = newPos;
 
             m_LastTargetPosition = target.position;
+        }
+        void FindPlayer (){
+            if (nextTimeToSearch <= Time.time){
+                GameObject searchResult = GameObject.FindGameObjectWithTag ("Player");
+                if (searchResult != null)
+                    target = searchResult.transform;
+                nextTimeToSearch = Time.time + 0.5f;
+            }
         }
     }
 }

f47536a2509972a826699203cfe476f05e174613
Cordell Bonnieux <cordellbonnieux@gmail.com>
2021-02-15 23:52:49 -0800

    Added respawn particle effect and audio countdown.

--- Assets/GameMaster.cs
+++ Assets/GameMaster.cs
@@ -13,14 +13,16 @@ public class GameMaster : MonoBehaviour
     }    
     public Transform playerPrefab;
     public Transform spawnPoint;
-    public int spawnDelay = 2;
+    public float spawnDelay = 2f;
+    public Transform spawnPrefab;
 
     public IEnumerator RespawnPlayer () {
-        Debug.Log ("TODO: Add waiting for spawn sound.");
+        GetComponent<AudioSource>().Play();
         yield return new WaitForSeconds (spawnDelay);
 
         Instantiate (playerPrefab, spawnPoint.position, spawnPoint.rotation);
-        Debug.Log ("TODO: Add Spawn Particles");
+        Transform clone = Instantiate(spawnPrefab, spawnPoint.position, spawnPoint.rotation) as Transform;
+        Destroy(clone.gameObject, 3f);
     }
     public static void KillPlayer (Player player) {
         Destroy (player.gameObject);
